d0dad8f45d3862b20d139d0f2f5fa71f4f2709af
Dina Grymak <dina@Dinas-MacBook-Pro.local>
2018-05-30 21:44:16 -0700

    minor changes

--- app/src/androidTest/java/com/example/dina/findrepo/Screens/SearchScreen.kt
+++ app/src/androidTest/java/com/example/dina/findrepo/Screens/SearchScreen.kt
@@ -1,19 +1,16 @@
 package com.example.dina.findrepo.Screens
 
-import android.support.test.InstrumentationRegistry
+
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.ViewInteraction
 import android.support.test.espresso.action.ViewActions.*
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.espresso.matcher.ViewMatchers.*
-import android.support.test.uiautomator.UiDevice
 import android.support.test.uiautomator.UiSelector
 import com.example.dina.findrepo.R
-import kotlinx.android.synthetic.main.activity_main.view.*
 import org.junit.Assert
-import org.junit.Before
-import java.util.*
+
 
 /**
  * Created by dina on 3/31/18.
@@ -28,9 +25,9 @@ class SearchScreen : BaseScreen() {
     val hintForUserField get() = UiSelector().resourceId("userRepoEditText").toString()
     val snackBar = uiDevice.findObject(UiSelector().resourceId("com.example.dina.findrepo:id/snackbar_text"))
 
-    init {
-        searchButton
-    }
+//    init {
+//        Assert.assertTrue("", searchButton.waitForExists())
+//    }
 
     fun clickSearchBtn() = searchButton.perform(click())
     fun clickViewBtn() = viewButton.perform(click())
